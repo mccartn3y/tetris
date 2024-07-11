5390ff284730f0dbbd564b19d5b22c542d00731e
Michael McCartney <michael@mccartn3y.com>
2024-07-11 18:59:12 +0200

    add tests to observer

--- src/turn_timer/observer.rs
+++ src/turn_timer/observer.rs
@@ -20,3 +20,63 @@ pub trait Subscriber<T: std::clone::Clone> {
     fn update(&mut self);
     fn add_subscription(&mut self, reciever: mpsc::Receiver<T>);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestNotifier<T> {
+        subscribers: Vec<mpsc::Sender<T>>,
+    }
+    impl<T: std::clone::Clone> Notifier<T> for TestNotifier<T> {
+        fn set_subscribers(&mut self) -> &mut Vec<mpsc::Sender<T>> {
+            &mut self.subscribers
+        }
+        fn subscribers(&self) -> &Vec<mpsc::Sender<T>> {
+            &self.subscribers
+        }
+    }
+
+    struct TestSubscriber {
+        value: String,
+        subscription: Option<mpsc::Receiver<String>>,
+    }
+    impl Subscriber<String> for TestSubscriber {
+        fn update(&mut self) {
+            if let Some(subscription) = &self.subscription {
+                if let Ok(value) = subscription.try_recv() {
+                    self.value = value;
+                }
+            }
+        }
+        fn add_subscription(&mut self, reciever: mpsc::Receiver<String>) {
+            self.subscription = Some(reciever);
+        }
+    }
+
+    #[test]
+    fn test_subscriber_works() {
+        let mut notifier: TestNotifier<String> = TestNotifier {
+            subscribers: Vec::new(),
+        };
+        let mut subscriber = TestSubscriber {
+            value: "".to_string(),
+            subscription: None,
+        };
+        notifier.add_subscriber(&mut subscriber);
+
+        assert_eq!(notifier.subscribers.len(), 1);
+
+        notifier.notify(&"notified!".to_string());
+        subscriber.update();
+        assert_eq!(subscriber.value, "notified!");
+    }
+
+    #[test]
+    fn test_notifier_does_nothing_with_no_subs() {
+        let notifier: TestNotifier<String> = TestNotifier {
+            subscribers: Vec::new(),
+        };
+        notifier.notify(&"notified!".to_string());
+    }
+}
